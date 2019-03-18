92c3d8765b411dd1ceae82da0a09551c6ce28c23
wangxiaoyun <wangxiaoyun@knowlink.com.cn>
2019-03-18 22:39:39 +0800

    2239

--- src/main/java/com/wxy/test/TestGson.java
+++ src/main/java/com/wxy/test/TestGson.java
@@ -7,21 +7,34 @@ import com.wxy.testneo4j.Device;
 import com.wxy.testneo4j.SpringBootNeo4jApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.*;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import static org.zeromq.ZMQ.REP;
+import static org.zeromq.ZMQ.context;
+
 
 public class TestGson {
     private final static Logger log = LoggerFactory.getLogger(TestGson.class);
 
+    public void testZmq()
+    {
+        Context context = context(1);
+        Socket responder = context.socket(REP);
+        while(!Thread.currentThread().isInterrupted())
+        {
+
+        }
+    }
+
     public static void main(String args[])
     {
         log.info("test gson start");
-
-
 
 
         List<Device> devices = new ArrayList<Device>();
